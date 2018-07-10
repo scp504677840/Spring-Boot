81e04ac66b0eab2d9395c1a59e243b555a18bf01
y1笑而过 <15335747148@163.com>
2018-07-10 15:40:36 +0800

    多环境日志配置
    多环境日志配置

---
+++ 日志/多环境日志配置/slab/src/main/java/com/slab/SlabApplication.java
@@ -0,0 +1,12 @@
+package com.slab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlabApplication.class, args);
+    }
+}
